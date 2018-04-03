55938b6b259c003fa9e0ae2c1aaf033fd4108d9c
Bernard Lee <bernard.hex@gmail.com>
2018-04-04 00:28:42 +0800

    Update Raw Data Assessments.py

--- recipes/Raw Data Assessments.py
+++ recipes/Raw Data Assessments.py
@@ -1,7 +1,16 @@
 def get_rows(file_path):
-    "Returns the sum of newlines from the total blocks"
+    """Returns the sum of newlines from the total blocks
+    
+    This function does not work with non text based files as far as testing goes.
+    Erros seems to be cause by count of newline while reading file.
+    More work needs to be done on reading different file types.
+    """
     with open(file_path, "r" ,errors = "ignore") as f:
-        print (sum(bl.count("\n") for bl in blocks(f)))
+        while True:
+            size = print (sum(bl.count("\n") for bl in blocks(f)))
+            if not size: print("error")
+            print(size)
+                
 
 def blocks(files,size = 65536):
     "Read file by defined sizes and returns a generator"
