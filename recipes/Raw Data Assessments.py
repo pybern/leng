df46ccc1ba4e9ca7a34ad81b369d0badcf27cd80
Bernard Lee <bernard.hex@gmail.com>
2018-04-04 00:20:51 +0800

    Update Raw Data Assessments.py

--- recipes/Raw Data Assessments.py
+++ recipes/Raw Data Assessments.py
@@ -1,14 +1,10 @@
 def get_rows(file_path):
-    '''
-    Returns the sum of newlines from the total blocks
-    '''
+    "Returns the sum of newlines from the total blocks"
     with open(file_path, "r" ,errors = "ignore") as f:
         print (sum(bl.count("\n") for bl in blocks(f)))
 
 def blocks(files,size = 65536):
-    '''
-    Read file by defined sizes and returns a generator
-    '''
+    "Read file by defined sizes and returns a generator"
     while True:
         b = files.read(size)
         if not b: break
