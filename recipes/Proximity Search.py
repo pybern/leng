115751e1261357966923d6b6236bfc74c96413a6
Bernard Lee <bernard.hex@gmail.com>
2018-06-06 17:34:20 +0800

    Update Proximity Search.py

--- recipes/Proximity Search.py
+++ recipes/Proximity Search.py
@@ -2,16 +2,20 @@ string = "some random string some random other"
 keywords = ['some','string','other']
 
 #test consecutive  occurence of different keywords in a string
+#lookup creates a graph styled network to trace the distance of words
 
 string = (i for i in string.split())
+lookup = []
 
 #might use tuples or generator instead of dictionary
 
 kv = {k:v for (k,v) in enumerate(string) if v in keywords}
+print(kv,'\n')
+
 
 for key,value in kv.items():
-    print(value)
-    a = next( ((k,v) for k,v in kv.items() if value != v and k > key), None )
-    print(a)
-    
+    a = next( ( (key,k,value,v)  for k,v in kv.items() if key < k < key+5) , None )
+    if a is not None:
+        lookup.append(a)
+    print('{}'.format( (key,value) ))
     
