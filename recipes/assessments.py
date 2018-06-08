a282c13c83a6d8deb47a8ae980be1517e7c3f9c0
Bernard Lee <bernard.hex@gmail.com>
2018-06-08 16:33:37 +0800

    Merged earlier file assessment

--- recipes/assessments.py
+++ recipes/assessments.py
@@ -1,3 +1,57 @@
+import zipfile
+import xlrd
+import io
+import glob
+import os
+
+# unzip all of the archive files in the provided path
+
+def unzip_archive(path):
+    
+    # Check everything with the file extension 'zip' in the provided path
+    files = glob.glob(path+'\\*.zip')
+    
+    # Extract all the files inside the archive
+    
+    for f in files:
+        zf = zipfile.ZipFile(f, mode='r')
+        output = input('Please provide the full path to store the unzipped files: ')
+        zf.extractall(path=output)
+        zf.close()
+    return get_ext(files,output)
+
+
+# To get an overview of the file types extracted from the archive file
+def get_ext(file_ext,outpath):
+    files=[]
+    count=0
+    for file_ext in os.listdir(outpath):
+        file_ext = file_ext.split('.')[1]
+        if file_ext not in files:
+            files.append(file_ext)
+    print('Here are the file types extracted from the archive file: ',files)
+    return summ_stat(outpath)  
+
+
+
+# To get the summary of the stastistic of all the file extracted from the archive file
+def summ_stat(out):
+    for file in os.listdir(out):
+        print('File Name: ',file)
+        num_lines=0
+        if file.endswith('txt'):
+            for line in file:
+                num_lines += 1
+            print('number of lines in the text file: ', num_lines)
+        elif file.endswith('xls') or file.endswith('xlsx'):
+            workbook = xlrd.open_workbook(out+'\\'+file)
+            for sheet in workbook.sheets():
+                print('The number of Rows: ',sheet.nrows)
+                print('The number of Columns: ',sheet.ncols)
+                        
+        else:
+            pass
+
 def get_rows(file_path):
     """Returns the sum of newlines from the total blocks
     
