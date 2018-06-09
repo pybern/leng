1c01271b026d14944cb158a0b1e292f7bc5d81cc
Bernard Lee <bernard.hex@gmail.com>
2018-06-09 18:54:57 +0800

    Update proximity.py

--- recipes/proximity.py
+++ recipes/proximity.py
@@ -1,53 +1,76 @@
 #string = "a a x x x a x x x "
-string = text
-keywords = ['is','a']
 
 #test consecutive  occurences of different keywords in a string
-
-_string = [i for i in string.split()]
-lookup = []
-
-#might use tuples or generator instead of dictionary
-
-kv = [ (k,v) for (k,v) in enumerate(_string) if v in keywords ]
-print(kv,'\n')
-
-
-for key,value in kv:
-    a = next( ( (key,k)  for k,v in kv if key < k < key+5) , None )
-
-    if a is not None:
-        lookup.append(a)
-    print(lookup)
-        
-print('\n',lookup,'\n\n',string)
-
-chains = []
-chain = []
-
-for i in lookup:
-    if len(lookup) == 1:
-        chain = list(i)
-        
-    else:
-        a = next( (i[1] for key in lookup if key[1] == i[0]) , None)
     
-        if a is None:
-            if len(chain) > 1:
-                chains.append(chain)
-            chain = []
-            chain.append(i[0])
+def build_lookup(string, keywords):
+    _string = [i for i in string.lower().split()]
+    lookup = []
+
+    #might use tuples or generator instead of dictionary
+
+    kv = [ (k,v) for (k,v) in enumerate(_string) if v in keywords ]
+    
+    for key,value in kv:
+        a = next( ( (key,k)  for k,v in kv if key < k < key+5) , None )
+
+        if a is not None:
+            lookup.append(a)
+            print(key,value,lookup)
+
+    return(lookup)
+    print(lookup)
+
+
+#manage here to, multilookup
+
+def build_chain(lookup):
+    chains = []
+    chain = []
+
+    for i in lookup:
+        if len(lookup) == 1:
+            chain.extend(i)
 
         else:
-            if len(chain) == 1:
-                chain.append(a)
-            else:
-                chain[1] = a
-        
-chains.append(chain)
-        
-    
-print(lookup,'\n\n',chains)
+            a = next( (i[1] for key in lookup if key[1] == i[0]) , None)
 
-for i in chains:
-    print(' '.join( _string[i[0]:i[1]+1] ) )
+            if a is None:
+                if len(chain) > 1:
+                    chains.append(chain)
+                chain = []
+                chain.append(i[0])
+
+            else:
+                if len(chain) == 1:
+                    chain.append(a)
+                else:
+                    chain[1] = a
+    
+    chains.append(chain)
+    
+    print(chains)
+    return(chains)
+    
+
+
+def preview(chains):
+    for i in chains:
+        start = i[0] - 5  if i[0] > 5 else i[0]
+        end = i[1]+6 if i[1]+1 < 5 else i[1]+1 
+        print(' '.join( _string[i[0]:i[1]+1] ) )
+        
+string = ''' This is our world now... the world of the electron and the switch , the
+beauty of the baud .  We make use of a service already existing without paying
+for what could be dirt-cheap if it wasn't run by profiteering gluttons, and
+you call us criminals.  We explore... and you call us criminals.  We seek
+after knowledge... and you call us criminals.  We exist without skin color,
+without nationality, without religious bias... and you call us criminals.
+You build atomic bombs, you wage wars, you murder, cheat, and lie to us
+and try to make us believe it's for our own good, yet we're the criminals.'''
+
+#keywords = input().split()
+keywords = ['electron','world']
+
+a = build_lookup(string, keywords)
+b = build_chain(a)
+c = preview(b)
